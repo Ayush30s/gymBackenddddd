f59a4596b00b94cb7229edb35c257d3bf6702116
Ayush Srivastav <ayushsri8318328947@gmail.com>
2025-06-07 23:41:09 +0530

    app. use fonrtned url

--- index.js
+++ index.js
@@ -69,11 +69,10 @@ io.on("connection", (socket) => {
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://gymfrontendd.netlify.app",
     credentials: true,
   })
 );
-
 app.use(cookieParser());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
